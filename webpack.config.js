a85497ec1f7a80911b07d6c56d5c6304fea74a63
Aaron Gebana <aarongebana@gmail.com>
2018-10-08 16:05:20 +0800

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "_build"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  resolve: {
+    alias: {
+      ["@src"]: path.resolve(__dirname, "src/"),
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack + React App",
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        exclude: [/\.(js|jsx)$/, /\.html$/, /\.ejs$/],
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name]_[hash:7].[ext]",
+              publicPath: "assets/images/",
+              outputPath: "assets/images/"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: [
+          {
+            loader: "image-webpack-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
