33b798b7fc39466ab5768afc7fcc71f8ee92d9ee
Aaron Gebana <aarongebana@gmail.com>
2018-10-09 17:55:12 +0800

    Move css files to src/css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import css from "./index.css";
+import "./css/Main";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("app"));
