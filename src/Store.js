1f38e0e9700b379e1fa29d31b4c83d572648d792
Aaron Gebana <aarongebana@gmail.com>
2018-10-09 13:39:22 +0800

    Setup devtools

--- src/Store.js
+++ src/Store.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { Provider as BaseStoreProvider } from "react-redux";
 import { createStore } from "redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
 import rootReducer from "./Reducer";
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  devToolsEnhancer()
+  // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+);
 
 export const Provider = ({ children }) => (
   <BaseStoreProvider store={store}>{children}</BaseStoreProvider>
