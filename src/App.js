c13638b3161ad60cc9b26bc9e68e7d59a00fa08f
Aaron Gebana <aarongebana@gmail.com>
2018-10-09 17:28:09 +0800

    Setup react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import { compose } from "recompose";
 import React from "react";
 import { hot } from "react-hot-loader";
-
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from "react-router-dom";
 import withProvider from "./Provider";
+import Home from "./screens/Home";
+import NotFound from "./screens/NotFound";
+
+const NoMatch = props => <Redirect to="/404" />;
 
 export default compose(
   hot(module),
   withProvider
-)(_props => <div>Hello, World!</div>);
+)(_props => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/404" component={NotFound} />
+      <Route component={NoMatch} />
+    </Switch>
+  </Router>
+));
---
+++ src/screens/NotFound.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default props => <div>Error 404: Not Found</div>;
