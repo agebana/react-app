d31aad5dec683ae1459cbdc34fbb6ef1b8c775c5
Aaron Gebana <aarongebana@gmail.com>
2018-10-10 15:57:42 +0800

    Change extensions to jsx

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { compose } from "recompose";
+import React from "react";
+import { hot } from "react-hot-loader";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from "react-router-dom";
+import withProvider from "./Provider";
+import NavigationBar from "@components/NavigationBar";
+import HomeScreen from "@screens/Home";
+import NotFoundScreen from "@screens/NotFound";
+import NotFound from "@screens/NotFound";
+
+const NoMatch = props => <Redirect to={NotFound.path} />;
+
+export default compose(
+  hot(module),
+  withProvider
+)(props => (
+  <Router>
+    <div>
+      <NavigationBar />
+      <Switch>
+        <Route exact path={HomeScreen.path} component={HomeScreen.component} />
+        <Route
+          exact
+          path={NotFoundScreen.path}
+          component={NotFoundScreen.component}
+        />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>
+));
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,9 +1,9 @@
-import React from "react";
 import styles from "@css/Home";
+import React from "react";
 
 export const path = "/";
 export const Component = props => (
-  <div className={styles.root}>This is the home screen!</div>
+  <div className={styles.root}>Home Screen</div>
 );
 
 export default {
