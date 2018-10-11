07c569ac95e0588d7d9bef53c3e842e5bc49a527
Aaron Gebana <aarongebana@gmail.com>
2018-10-11 16:30:21 +0800

    Checkpoint

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,7 +3,19 @@ import React from "react";
 
 export const path = "/";
 export const Component = props => (
-  <div className={styles.root}>Home Screen</div>
+  <div className={styles.root}>
+    <div className={styles.sideSection}>
+      <div className={styles.sideMenu}>Side menu goes here</div>
+    </div>
+    <div className={styles.content}>
+      <h1>Content Goes Here</h1>
+      <h2>This is a section header</h2>
+      <p>This is a paragraph</p>
+      <h2>This is a section header</h2>
+      <p>This is a paragraph</p>
+    </div>
+    <div className={styles.sideSection}>Right</div>
+  </div>
 );
 
 export default {
