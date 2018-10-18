d7ed3b3fb8680e1fcc5597ce11f0578967509311
Aaron Dwayne Gebana <agebana@corp.nextdigital.com>
2018-10-18 14:10:43 +0800

    Configure store for redux-saga

--- src/Store.js
+++ src/Store.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Provider as BaseStoreProvider } from "react-redux";
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
 import rootReducer from "./Reducer";
+import rootSaga from "./Saga";
+
+const composeEnhancers = composeWithDevTools({
+  // Specify name here, actionsBlacklist, actionsCreaters and other options if needed
+});
+
+const middleware = [createSagaMiddleware()];
 
 const store = createStore(
   rootReducer,
-  devToolsEnhancer()
+  composeEnhancers(applyMiddleware(...middleware))
   // Specify name here, actionsBlacklist, actionsCreators and other options if needed
 );
 
